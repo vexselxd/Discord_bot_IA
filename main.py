cb135612a6c561afc2d9ee9e951a7ac6c30dedd2
vexselxd <medinillavalenzuela@gmail.com>
2025-06-13 17:15:52 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from model import get_model
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def cargar(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            fileurñl = attachment.url
+            await attachment.save(f"./files/{file_name}")
+            await ctx.send(get_model(model_path = "./keras_model.h5", labels_path = "./labels.txt", image_path = f"./files/{file_name}"))
+    else:
+        await ctx.send("No hay archivos adjuntos")
+bot.run("Token")
